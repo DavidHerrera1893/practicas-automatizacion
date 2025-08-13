72a64e42a9889796da14d92eceb8cbdb45170d68
David Herrera <davidherrera-00@outlook.com>
2025-08-13 11:13:29 -0500

    Segundo script en Python: variables y print

---
+++ variables_y_print.py
@@ -0,0 +1,15 @@
+# Esto es un comentario: no se ejecuta, sirve para explicar el código
+
+# Guardamos información en variables
+nombre = "David"
+edad = 32
+es_QA = True  # True significa "sí" o "verdadero"
+
+# Mostramos en pantalla el contenido de las variables
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Es QA:", es_QA)
+
+# Hacemos un cálculo con variables
+anios_para_jubilarse = 65 - edad
+print("Años para jubilarse:", anios_para_jubilarse)
